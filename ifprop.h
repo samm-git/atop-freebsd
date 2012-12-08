ccca3ee3f5e75a8fd1c0335bf7de42b95798f5cb
Alex Samorukov <samm@os2.kiev.ua>
2012-12-08 19:52:44 +0100

    deleting trash

--- ifprop.h
+++
@@ -1,8 +0,0 @@
-struct ifprop	{
-	char		name[16];	/* name of interface  		*/
-	long int	speed;		/* in megabits per second	*/
-	char		fullduplex;	/* boolean			*/
-};
-
-int 	getifprop(struct ifprop *);
-void 	initifprop(void);
